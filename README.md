1254ad7ae2578bcc171f985431a87e6a80471bc8
Greg Stephens <greg@udon.org>
2017-01-07 09:36:52 -0800

    List view with Griddle

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ A starting point for Meteor apps.
 </table>
 
 [Read the Documentation](http://themeteorchef.com/base)
+
+# ListView
+
+This extension to [Meteor Base](http://themeteorchef.com/base) enhances the existing List Views to add pagination, sorting and filtering using [Griddle](http://griddlegriddle.github.io/Griddle/). Install Griddle via npm and then update DocumentList.js.
+
+```
+meteor npm install griddle-react --save
+```
--- imports/ui/components/DocumentsList.js
+++ imports/ui/components/DocumentsList.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, Alert } from 'react-bootstrap';
+import { Button, ButtonGroup, Alert } from 'react-bootstrap';
+import { removeDocument } from '../../api/documents/methods';
+import { browserHistory } from 'react-router';
+import Griddle from 'griddle-react';
+
+const handleRemove = (_id) => {
+  if (confirm('Are you sure? This is permanent!')) {
+    removeDocument.call({ _id }, (error) => {
+      if (error) {
+        Bert.alert(error.reason, 'danger');
+      } else {
+        Bert.alert('Document deleted!', 'success');
+        browserHistory.push('/documents');
+      }
+    });
+  }
+};
+
+var actionButtons = React.createClass({
+  render: function () {
+    return (
+        <div>
+          <ButtonGroup>
+            <Button bsSize="xsmall" href={'/documents/' + this.props.rowData._id}>View</Button>
+            <Button bsSize="xsmall" href={'/documents/' + this.props.rowData._id + '/edit'}>Edit</Button>
+            <Button bsSize="xsmall" onClick={ () => handleRemove(this.props.rowData._id) } bsStyle="danger">Delete</Button>
+          </ButtonGroup>
+        </div>
+    );
+  }
+});
+
+const columnList = ['title','body','actions'];
+const columnMetadata = [
+  {columnName: 'title', order: "1", displayName: 'Title', cssClassName: 'col-xs-2' },
+  {columnName: 'body', order: "2", displayName: 'Body', cssClassName: 'col-xs-8' },
+  {columnName: 'actions', order: "3", customComponent: actionButtons, sortable: false, cssClassName: 'col-xs-2',
+    displayName: 'Actions'}
+];
 
 const DocumentsList = ({ documents }) => (
-  documents.length > 0 ? <ListGroup className="DocumentsList">
-    {documents.map(({ _id, title }) => (
-      <ListGroupItem key={ _id } href={`/documents/${_id}`}>{ title }</ListGroupItem>
-    ))}
-  </ListGroup> :
+    documents.length > 0 ?
+        <Griddle
+            results={documents}
+            useGriddleStyles={false}
+            tableClassName={'griddle-flex table table-bordered table-striped table-hover'}
+            settingsToggleClassName='btn btn-default'
+            useCustomPagerComponent={false}
+            showFilter={true}
+            showSettings={true}
+            resultsPerPage={50}
+            columnMetadata={columnMetadata}
+            columns={columnList}/> :
   <Alert bsStyle="warning">No documents yet.</Alert>
 );
 
